2f335eb8b960974b09b1154704714b0a927fe744
Thomas del Mundo <tjdm329@gmail.com>
2026-03-14 23:22:08 -0400

    week 9 lab 6 complete

---
+++ wk9/Lab06_101498572/exercise-1/hello-world.js
@@ -0,0 +1,4 @@
+var greeter = function (first, last) {
+    console.log("Hello ".concat(first, " ").concat(last));
+};
+greeter("John", "Smith");
---
+++ wk9/Lab06_101498572/exercise-4/customer.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Customer = void 0;
+var Customer = /** @class */ (function () {
+    function Customer(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    Customer.prototype.greeter = function () {
+        console.log("Hello ".concat(this.firstName, " ").concat(this.lastName));
+    };
+    Customer.prototype.getAge = function () {
+        console.log("Age: ".concat(this.age));
+        return this.age;
+    };
+    return Customer;
+}());
+exports.Customer = Customer;
